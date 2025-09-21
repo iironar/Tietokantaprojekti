ee4f18e0f31fdc0085e45491205bfedfbfd1566c
Iiro <iiro.narvanen@gmail.com>
2025-09-21 23:41:22 +0300

    Added ability to create a sale announcement

--- app.py
+++ app.py
@@ -13,6 +13,21 @@ def index():
     return render_template("index.html")
     
 
+@app.route("/new_item")
+def new_item():
+    return render_template("new_item.html")
+
+@app.route("/create_item", methods=["POST"])
+def create_item():
+    title = request.form["title"]
+    price = request.form["price"]
+    description = request.form["description"]
+    user_id = session["user_id"]
+    
+    sql = "INSERT INTO items (title, price, description, user_id) VALUES (?, ?, ?, ?)"
+    db.execute(sql, [title, price, description, user_id])
+    return redirect("/")
+
 @app.route("/login", methods=["GET","POST"])
 def login():
     
@@ -24,10 +39,14 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
         
-        sql = "SELECT password_hash FROM users WHERE username = ?"
-        password_hash = db.query(sql, [username])[0][0]
+        sql = "SELECT id, password_hash FROM users WHERE username = ?"
+        result = db.query(sql, [username])[0]
+        user_id = result["id"]
+        password_hash = result["password_hash"]
+        
 
         if check_password_hash(password_hash, password):
+            session["user_id"] = user_id 
             session["username"] = username
             return redirect("/")
         else:
@@ -36,6 +55,7 @@ def login():
 @app.route("/logout")
 def logout():
     del session["username"]
+    del session["user_id"]
     return redirect("/")
 
 @app.route("/register")
